5c5b3224c214aafc1b8fd5853311fd4298bf27d0
verb0 <verb0@users.noreply.github.com>
2014-09-29 15:14:05 +0200

    Update SecureAdditionServer.java

--- SecureAdditionServer.java
+++ SecureAdditionServer.java
@@ -1,3 +1,5 @@
+package SSLsocket;
+
 
 // An example class that uses the secure server socket class
 // SSL/TLS: Secure Sockets Layer, predecessor to Transport Layer Security, 
@@ -23,7 +25,7 @@ public class SecureAdditionServer {
 	static final String STOREPASSWD = "changeit";
 	static final String ALIASPASSWD = "changeit";
 	
-	public final static String FILE_TO_SEND = "hej.txt";  // you may change this
+	public final static String FILE_TO_SEND = "hej.txt";  // file to be sent
 	
 	/** Constructor
 	 * @param port The port where the server
@@ -59,6 +61,8 @@ public class SecureAdditionServer {
 			SSLContext sslContext = SSLContext.getInstance( "TLS" );
 			sslContext.init( kmf.getKeyManagers(), tmf.getTrustManagers(), null );
 			SSLServerSocketFactory sslServerFactory = sslContext.getServerSocketFactory();
+			
+			//Secure server socket sss
 			SSLServerSocket sss = (SSLServerSocket) sslServerFactory.createServerSocket( port );
 			sss.setEnabledCipherSuites( sss.getSupportedCipherSuites() );
 			
@@ -67,21 +71,42 @@ public class SecureAdditionServer {
 			
 			
 		  // send file (http://www.rgagnon.com/javadetails/java-0542.html)
-          File myFile = new File (FILE_TO_SEND);
+			
+          //Create a new file to send
+		  File myFile = new File (FILE_TO_SEND);
+		  
+          //File size
           byte [] mybytearray  = new byte [(int)myFile.length()];
+          
+          //Input bytes from a file, read stream of raw bytes 
           fis = new FileInputStream(myFile);
+          
+          //Buffer: memory used to temporarily store data while it's being moved from one place to another.
+          //Add functionality to stream fis
           bis = new BufferedInputStream(fis);
-          bis.read(mybytearray,0,mybytearray.length);
+          
+          //Reads bytes from bis into mybytearray starting at 0
+          bis.read(mybytearray,0,mybytearray.length);	//Want to read from bis the array from first position to the last.
+          
+          
+          //Set output stream to the incoming stream
+          //incoming = incoming ssl socket
           os = incoming.getOutputStream();
+          
           System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+          
+          //Write to outputstream os from mybytearray, starting from position 0
           os.write(mybytearray,0,mybytearray.length);
+          //Flushes this output stream and forces any buffered output bytes to be written out. (print everything in os)
           os.flush();
           System.out.println("Done.");
           
-          	bis.close();
-			os.close();
-			incoming.close();
-			sss.close();
+          //Close all streams and release any system resources assouciated with the streams
+          bis.close();
+          os.close();
+          //Close all sockets
+          incoming.close();
+          sss.close();
 			
 			//Get file from client
 			
